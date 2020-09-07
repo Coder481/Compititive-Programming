1b543d873acf3e8a2aa549eb980f73954f6d9a8d
Coder481 <68111551+Coder481@users.noreply.github.com>
2020-09-07 17:01:07 +0530

    Create If-else.py

---
+++ Hackerrank/python if-else/If-else.py
@@ -0,0 +1,11 @@
+#https://www.hackerrank.com/challenges/py-if-else/problem
+n=int(input())
+if n%2!=0:
+    print "Weird"
+else:
+    if 2<=n<=5:
+        print 'Not Weird'
+    elif 6<=n<=20:
+        print 'Weird'
+    else:
+        print 'Not Weird'
